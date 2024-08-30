6504b3b3c76dab5743b6db6f9742721371981210
shumilov <wumilovsergey@gmail.com>
2024-08-30 13:21:08 +0300

    up

--- api/middleware.py
+++ api/middleware.py
@@ -1,79 +1,23 @@
-from django.shortcuts import redirect
-import secrets
-from .models import TelegramUsers
-from django.utils import timezone
+import uuid
 
-class CheckSessionMiddleware:
+class CSPNonceMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-        if "bot" in request.path or "auth_check" in request.path:
-            print("")
-            print("SessionMiddll ignore /bot or /auth")
-            print("")
-            response = self.get_response(request)
-            return response
-
-        if "session_id" in request.session and TelegramUsers.objects.filter(session_id=request.session["session_id"]).exists():
-            user = TelegramUsers.objects.get(session_id=request.session["session_id"])
-            request.session["auth"] = True
-            request.session["name"] = user.name
-            
-        else:
-            session_id = secrets.token_hex(16)
-            while TelegramUsers.objects.filter(session_id=session_id).exists():
-                session_id = secrets.token_hex(16)
-            request.session["session_id"] = session_id
-            request.session["auth"] = None
-            request.session["name"] = None
-
+        nonce = uuid.uuid4().hex
+        request.csp_nonce = nonce
 
         response = self.get_response(request)
-        return response
-    
 
-class SessionDebuger:
-    def __init__(self, get_response):
-        self.get_response = get_response
+        csp = (
+            f"script-src 'self' https://telegram.org 'nonce-{nonce}'; "
+            f"style-src 'self' 'nonce-{nonce}';"  # Example for allowing inline styles
+        )
 
-    def __call__(self, request):
-        if "bot" in request.path or "auth_check" in request.path:
-            print("")
-            print("SessionDebbugerMidd ignore /bot in request.path")
-            print("")
-            response = self.get_response(request)
-            return response
-        
-        session_id = request.session["session_id"]
-        auth = request.session["auth"]
-        name = request.session["name"]
-
-
-        print("##_______SESSION____##")
-        print(f"session_id - {session_id}")
-        print(f"auth       - {auth}")
-        print(f"name       - {name}")
-        print(" ")
-        print(" ")
-
-        path_exceptions = [
-            "/",
-            "/bot",
-            "/login/",
-            "/logout/",
-            "/auth_check/",
-        ]
-
-        if request.path in path_exceptions or request.session["auth"] or "/admin" in request.path:
-            response = self.get_response(request)
-            return response
-        
+        if 'Content-Security-Policy' in response:
+            response['Content-Security-Policy'] += f" {csp}"
         else:
-            print(" ")
-            print("##_______REDIRECT____##")
-            print(f"SessionDebugerMidd redirect {request.path} to the /")
-            print("exception path")
-            print("")
-            print("")
-            return redirect("/")
+            response['Content-Security-Policy'] = csp
+
+        return response
